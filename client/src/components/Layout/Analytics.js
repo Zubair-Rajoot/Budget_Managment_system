9fa0137c707a30607616e2fe71f1e12e3b1082e4
maaza12 <maaza1237@gmail.com>
2024-11-09 16:06:11 +0500

    add graps of turnover and transaction

--- client/src/components/Layout/Analytics.js
+++ client/src/components/Layout/Analytics.js
@@ -3,6 +3,11 @@ import { Progress } from "antd";
 
 const Analytics = ({ allTransection }) => {
 
+  const totalTurnover = allTransection.reduce((acc,transaction) => acc + transaction.amount, 0);
+  const totalincomeTurnover = allTransection.filter(transaction => transaction.type === 'income').reduce((acc,transaction)=> acc + transaction.amount,0);
+  const totalExpenseTurnover = allTransection.filter(transaction => transaction.type === 'expense').reduce((acc,transaction) => acc + transaction.amount,0);
+  const totalIncomeTurnoverPercent = (totalExpenseTurnover/ totalTurnover) * 100
+  const totalExpenseTurnoverPercent = (totalExpenseTurnover/totalTurnover) * 100
 
   // total transaction
   const totalTransaction = allTransection.length;
@@ -44,7 +49,32 @@ const Analytics = ({ allTransection }) => {
             </div>
           </div>
         </div>
-      </div>
+      <div className="col-md-4">
+          <div className="card">
+            <div className="card-header">
+              Total TurnOver : {totalTurnover}
+            </div>
+            <div className="card-body">
+              <h5 className="text-success">Income: {totalincomeTurnover}</h5>
+              <h5 className="text-danger">Expense: {totalExpenseTurnover}</h5>
+              <div>
+                <Progress
+                  type="circle"
+                  strokeColor="green"
+                  className="mx-2"
+                  percent={totalIncomeTurnoverPercent.toFixed(0)}
+                />
+                <Progress
+                  type="circle"
+                  strokeColor="red"
+                  className="mx-2"
+                  percent={totalExpenseTurnoverPercent.toFixed(0)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
     </>
   );
 };
